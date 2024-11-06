db7228a33308046cd3441bf3e6aab38e00ec8a14
Jhonatan <jhonatanramosleite21@gmail.com>
2024-11-06 17:03:49 -0300

    movimentacao

--- tela_inicial.py
+++ tela_inicial.py
@@ -36,9 +36,15 @@ while game:
         if event.type == QUIT:
             pygame.quit()
             exit()
-     
-            
-
+        if event.type == KEYDOWN():
+            if event.key == pygame.K_LEFT:
+                movimentacao = Jogador.movimentar("esquerda")
+            elif event.key == pygame.K_RIGHT:
+                movimentacao = Jogador.movimentar("direita")
+            elif event.key == pygame.K_UP:
+                movimentacao = Jogador.movimentar("cima")
+            elif event.key == pygame.K_DOWN:
+                movimentacao = Jogador.movimentar("baixo")
     # Desenhar a imagem de fundo
     screen.blit(image, (0, 0)) 
 
