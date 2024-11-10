18b9c3257b3b58cf6a133cdec388c68a403631f6
Jhonatan <jhonatanramosleite21@gmail.com>
2024-11-10 15:25:59 -0300

    mapa integrado ao jogo

--- classes.py
+++ classes.py
@@ -1,30 +1,18 @@
 
-import constantes 
+from constantes import *
 import pygame
-#configurações iniciais 
-# pygame.init()
-# pygame.mixer.init()
-# screen = pygame.display.set_mode((constantes.LARGURA, constantes.ALTURA))
-# pygame.display.set_caption(constantes.TITULO)
-# clock = pygame.time.Clock()
+import constantes
 
     
 # Classe Tile (representa um caminho)
+# Atualiza a classe Tile para usar as imagens diretamente
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, row, column):
-        # Inicializa a classe base Sprite
+    def __init__(self, tile_img, row, column):
         super().__init__()
-        self.tile_type = tile_type  # Define o tipo de tile: "wall" (parede) ou "path" (caminho)
-
-        # Cria uma superfície para o tile e define a cor
-        self.image = pygame.Surface((constantes.TAMANHO_QUADRADO, constantes.TAMANHO_QUADRADO))
-        self.image.fill(constantes.PRETO if tile_type == "wall" else constantes.PRETO)  # Preto para parede,preto para caminho
-
-        # Obtém o retângulo de posição do tile e define sua posição na grade
+        self.image = tile_img  # A imagem já é carregada no assets
         self.rect = self.image.get_rect()
-        self.rect.x = column * constantes.TAMANHO_QUADRADO  # Posição X com base na coluna e no tamanho do tile
-        self.rect.y = row * constantes.TAMANHO_QUADRADO     # Posição Y com base na linha e no tamanho do tile
-        
+        self.rect.x = column * TAMANHO_QUADRADO  # Posição X com base na coluna e no tamanho do tile
+        self.rect.y = row * TAMANHO_QUADRADO     # Posição Y com base na linha e no tamanho do tile
 # Classe Obstáculo (obstáculo que "morre" ao colidir)
 class Obstaculo(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -58,20 +46,6 @@ class Jogador:
     def desenhar(self, screen):
         # Desenha o jogador como um quadrado azul
         pygame.draw.rect(screen, constantes.AZUL, (self.posicao.x, self.posicao.y, 40, 40))
-
-        # def novo_jogo(self):
-        #     self.all.sprites = pygame.sprite.Group()
-        #     self.rodar()
-        
-        # #loop do jogo    
-        # def rodar(self):
-        #     self.jogando = True 
-        #     while self.jogando:
-        #         self.clock.ticket(constantes.FPS)
-        #         self.eventos()
-        #         self.refresh_sprites()
-        #         self.desenhar_sprites()
-            
     
     
 
