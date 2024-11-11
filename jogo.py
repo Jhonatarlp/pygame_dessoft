e78acb45bc22f209c929e6c5b27d3b711c5f2ec4
Marlize Debossan <marlizeldc@al.insper.edu.br>
2024-11-11 17:17:13 -0300

    jogo.v4

--- jogo.py
+++ jogo.py
@@ -16,11 +16,25 @@ def carrega_assets():
         'vacuo': pygame.image.load(IMG_DIR / 'vacuo.png').convert_alpha(),
         'caminho': pygame.image.load(IMG_DIR / 'caminho.png').convert_alpha(),
         'fim': pygame.image.load(IMG_DIR / 'fim1.png').convert_alpha(),
-        'jogador': pygame.image.load(IMG_DIR / 'foxy1.png').convert_alpha(),  
+        # 'jogador': pygame.image.load(IMG_DIR / 'foxy1.png').convert_alpha(),  
     }
     # Redimensiona as imagens para o tamanho do tile
     for key in assets:
         assets[key] = pygame.transform.scale(assets[key], (TAMANHO_QUADRADO, TAMANHO_QUADRADO))
+
+    foxy_anim = []
+    for i in range(1,5):
+        filename = f'foxy{i}.png'
+        img =  pygame.image.load(IMG_DIR / filename).convert_alpha()
+        img = pygame.transform.scale(img, (20, 20))  # Redimensiona 
+        foxy_anim.append(img)
+    assets['anim foxy'] = foxy_anim
+
+    bolinha = 'bolinha.PNG'
+    img =  pygame.image.load(IMG_DIR / bolinha).convert_alpha()
+    img = pygame.transform.scale(img, (20, 20))  # Redimensiona 
+    assets['bolinha'] = img
+
     return assets
 
 # Loop principal do jogo
@@ -32,12 +46,12 @@ def game_loop(janela, assets):
         for coluna in range(len(MAPA1[linha])):
             tipo_quadrado = MAPA1[linha][coluna]
             if tipo_quadrado in assets:
-                quadrado = Tile(assets[tipo_quadrado], linha, coluna)
+                quadrado = Tile(assets[tipo_quadrado], linha, coluna, tipo_quadrado)
                 mapa_tiles.add(quadrado)
                 if tipo_quadrado == 'muro': 
                     grupo_obstaculos.add(quadrado)
 
-    jogador = Jogador(x_inicial, y_inicial, 5, assets['jogador'])
+    jogador = Jogador(x_inicial, y_inicial, 10, assets)
     #game_started = True 
     running = True
 
@@ -48,19 +62,19 @@ def game_loop(janela, assets):
                 exit()
             if event.type == KEYDOWN:
                 print(f"Tecla pressionada: {event.key}")  # Adicione esta linha para verificar o teclado
-                if event.key == pygame.K_UP:
-                    jogador.direcao = "cima"
-                elif event.key == pygame.K_DOWN:
-                    jogador.direcao = "baixo"
-                elif event.key == pygame.K_LEFT:
-                    jogador.direcao = "esquerda"
-                elif event.key == pygame.K_RIGHT:
-                    jogador.direcao = "direita"
-            if event.type == pygame.KEYUP:
-                jogador.direcao = None
+                if jogador.direcao == 'parado':
+                    if event.key == pygame.K_UP:
+                        jogador.direcao = "cima"
+                    elif event.key == pygame.K_DOWN:
+                        jogador.direcao = "baixo"
+                    elif event.key == pygame.K_LEFT:
+                        jogador.direcao = "esquerda"
+                    elif event.key == pygame.K_RIGHT:
+                        jogador.direcao = "direita"
+
         #desenha mapa e jogador
         #if game_started:
-        jogador.movimentar(MAPA1)  ####obs###
+        jogador.movimentar(grupo_obstaculos)  ####obs###
         janela.fill(PRETO)
         mapa_tiles.draw(janela) 
         jogador.desenhar(janela)  
