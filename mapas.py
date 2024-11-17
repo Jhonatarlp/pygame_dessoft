836141bb34e4da6d7e41e342936a02ef731af1da
ramelillo <rafaelaam1@al.insper.edu.br>
2024-11-17 19:05:07 -0300

    novo mapa 3

--- mapas.py
+++ mapas.py
@@ -42,29 +42,6 @@ MAPA1 = [
 ]
 
 MAPA2 = [
-    [v , v , v , v , v , v , v , v , v , v , v , v , v , m , m , m , m , m , m , m , m , m , m , m , v , v , v ],
-    [v , v , v , v , v , v , v , v , v , v , m , m , m , m , p , p , p , p , m , m , p , p , p , m , m , v , v ],
-    [v , v , v , v , v , v , v , v , v , v , m , p , p , p , p , p , p , p , m , m , p , m , p , m , m , v , v ],
-    [v , v , v , v , v , v , v , v , v , v , m , p , m , m , p , c , c , p , m , p , p , m , p , m , m , m , m ],
-    [v , v , v , v , v , v , v , v , v , m , m , p , m , m , p , p , p , p , m , p , p , p , p , p , p , m , m ],
-    [v , v , v , v , v , v , v , v , v , m , p , p , m , m , p , m , m , m , m , p , c , c , c , c , p , m , m ],
-    [m , m , m , m , m , m , m , v , v , m , p , m , m , m , p , p , p , p , p , p , p , p , p , p , p , m , m ],
-    [m , c , c , c , c , c , m , v , v , m , p , m , v , m , m , m , m , p , p , p , p , p , p , p , p , m , v ],
-    [m , c , c , c , c , c , m , v , v , m , p , m , v , v , v , v , m , m , m , m , m , m , p , m , m , m , v ],
-    [m , c , c , i , c , c , m , v , v , m , p , m , m , m , m , m , m , m , m , m , m , m , p , m , v , v , v ],
-    [m , m , m , m , m , p , m , v , v , m , p , p , p , m , p , p , p , p , p , p , m , m , p , m , m , m , v ],
-    [v , v , v , v , m , p , m , m , m , m , m , m , p , m , p , c , c , p , c , p , m , m , p , m , f , m , v ],
-    [v , v , v , v , m , p , m , m , p , p , p , m , p , m , p , c , c , p , c , p , m , m , p , m , p , m , v ],
-    [v , v , v , v , m , p , m , m , p , c , p , m , p , m , p , c , c , p , c , p , m , m , p , p , p , m , v ],
-    [v , v , v , v , m , p , p , p , p , p , p , m , p , p , p , p , p , p , p , p , m , m , m , m , m , m , v ],
-    [v , v , v , v , m , m , m , m , p , m , m , m , m , m , p , c , c , p , m , m , m , v , v , v , v , v , v ],
-    [v , v , v , v , v , v , v , m , p , p , p , p , p , m , p , c , c , p , m , v , v , v , v , v , v , v , v ],
-    [v , v , v , v , v , v , v , m , m , m , m , m , p , p , p , p , p , p , m , v , v , v , v , v , v , v , v ],
-    [v , v , v , v , v , v , v , v , v , v , v , m , m , p , p , p , p , p , m , v , v , v , v , v , v , v , v ],
-    [v , v , v , v , v , v , v , v , v , v , v , v , m , m , m , m , m , m , m , v , v , v , v , v , v , v , v ]
-]
-
-MAPA3 = [
     [v , v , v , v , v , v , v , v , v , v , v , v , v , v , m , m , m , m , m , m , m , m , m , v , v , v , v , v , v , v , v , v , v ],
     [v , v , v , v , v , v , v , v , v , v , v , v , m , m , m , p , p , p , m , p , p , p , m , m , m , m , v , v , v , v , v , v , v ],
     [v , v , v , v , v , v , v , v , v , v , m , m , m , f , m , p , m , p , m , p , m , p , p , p , p , m , v , v , v , v , v , v , v ],
@@ -86,6 +63,46 @@ MAPA3 = [
     [v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , m , m , m , m , m , m , m , v , v , v , v , v , v , v , v ]    
 ]
 
-MAPAX = [
-    [v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , v , v]
+MAPA3 = [
+    [m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, v, v, v, v, v, v, v, v, m, m, m, m, m, m, m, v, v, v, v, v, v, v],
+    [m, f, p, p, p, p, p, p, p, m, p, m, p, p, m, p, p, m, m, m, m, m, m, m, m, m, m, p, p, p, p, p, m, m, m, m, m, m, v, v],
+    [m, m, m, m, m, p, m, m, p, m, p, m, p, p, m, m, p, p, p, p, p, p, p, p, p, p, p, p, m, m, m, p, p, p, p, p, p, m, m, v],
+    [m, p, p, p, m, p, m, p, p, p, p, e, p, p, p, p, m, m, m, m, m, m, m, m, m, m, m, m, m, p, p, m, m, m, m, m, p, p, m, m],
+    [m, p, m, p, m, p, m, m, m, m, m, p, m, m, p, p, m, p, p, p, p, p, p, p, p, p, p, p, m, p, p, p, p, p, p, m, m, p, p, m],
+    [m, p, m, p, m, p, p, p, p, p, m, p, p, p, m, p, m, m, m, m, m, m, m, m, m, m, m, p, m, p, m, m, m, m, p, p, m, p, p, m],
+    [m, p, m, p, m, m, m, m, m, p, m, p, p, p, m, p, p, p, p, p, p, p, p, p, p, p, m, p, m, p, m, p, p, e, m, p, m, m, p, m],
+    [m, p, m, p, p, p, p, p, p, p, e, m, m, m, m, m, m, m, m, m, p, m, m, m, p, m, m, p, m, p, m, p, m, m, m, p, m, p, p, m],
+    [m, p, m, m, m, m, m, m, m, p, p, p, p, p, m, e, p, p, p, p, p, m, e, p, p, m, p, p, m, p, m, p, m, p, p, p, m, m, p, m],
+    [m, p, m, p, p, p, p, p, m, m, m, m, m, p, m, m, m, m, m, m, m, m, m, m, m, m, p, m, m, p, m, p, m, p, p, m, p, p, p, m],
+    [m, p, m, p, e, p, p, p, p, p, p, p, m, p, m, p, p, p, p, p, p, m, p, m, e, m, p, p, p, p, p, p, m, p, m, p, p, m, p, m],
+    [m, p, m, p, e, m, m, m, m, m, m, p, m, p, m, p, m, m, m, m, p, m, p, m, p, m, p, m, m, m, m, m, m, p, m, e, e, m, p, m],
+    [m, p, m, p, e, m, p, p, p, p, p, p, m, p, m, p, m, p, e, m, p, m, p, m, m, m, p, m, p, p, p, p, p, p, p, m, m, m, p, m],
+    [m, p, m, p, e, m, p, e, p, p, m, m, m, p, m, p, m, p, e, m, p, m, p, p, p, m, p, m, p, m, m, m, m, m, m, m, p, p, p, m],
+    [m, p, m, p, e, m, p, p, p, m, p, p, p, p, m, p, m, p, e, m, p, m, p, m, p, m, p, m, p, m, p, p, p, p, p, p, p, m, m, m],
+    [m, p, m, p, e, m, p, p, m, p, p, m, p, m, m, p, m, p, e, m, p, m, p, m, p, p, p, m, p, m, p, p, m, p, m, m, p, m, p, m],
+    [m, p, m, p, e, m, p, m, p, p, m, p, p, m, c, m, m, p, m, m, p, m, p, m, m, m, m, m, p, m, m, p, m, p, m, p, p, m, p, m],
+    [m, p, m, p, e, m, m, p, p, m, p, p, m, p, p, p, m, p, p, p, p, m, p, p, p, p, p, p, p, e, m, p, m, p, m, m, p, m, p, m],
+    [m, p, m, p, e, m, p, p, m, m, p, m, p, p, p, p, m, m, m, m, m, m, p, m, p, m, m, m, e, m, p, p, m, p, m, p, p, m, p, m],
+    [m, p, m, p, e, m, p, m, p, p, p, p, p, p, c, p, p, m, p, p, p, m, p, m, p, m, p, p, p, m, p, m, m, m, m, p, m, m, p, m],
+    [e, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, m],
+    [m, p, m, p, m, p, e, m, p, m, p, m, p, p, p, p, p, m, p, m, p, m, e, m, p, p, p, m, p, m, p, m, m, m, m, m, m, m, p, m],
+    [m, p, m, p, m, p, m, m, p, m, p, m, p, m, m, m, m, m, p, m, p, m, m, p, p, m, m, m, p, m, p, p, p, p, p, p, p, m, p, m],
+    [m, p, m, p, m, p, p, p, p, m, p, m, p, m, e, m, e, m, p, m, p, p, p, p, m, m, p, p, p, m, m, m, m, p, m, m, m, m, p, m],
+    [m, p, m, p, m, m, p, m, p, m, p, m, p, p, p, p, p, p, p, m, m, m, m, m, m, p, p, m, p, p, p, e, m, p, m, p, p, p, p, m],
+    [m, p, m, p, m, p, p, m, p, m, p, m, m, m, p, m, p, m, m, m, p, p, p, p, p, p, m, m, m, m, m, m, m, p, m, p, m, m, m, m],
+    [m, p, m, p, m, p, m, m, p, m, p, m, p, p, p, p, p, p, p, m, p, m, m, m, m, p, p, e, m, p, p, p, m, p, m, p, p, p, p, m],
+    [m, p, m, p, m, p, p, m, p, p, p, m, p, m, p, m, p, m, p, m, p, p, p, p, p, m, m, m, m, p, m, p, m, p, m, m, m, p, p, m],
+    [m, p, m, p, m, m, p, m, m, m, m, m, p, m, p, m, p, m, p, m, m, m, m, m, p, p, m, e, m, p, m, p, p, p, p, p, m, e, e, m],
+    [m, p, m, p, m, p, p, p, p, p, p, m, p, m, p, m, p, m, p, p, p, p, p, m, m, p, m, p, m, p, m, m, m, m, m, p, m, m, m, m],
+    [m, p, m, p, m, p, m, m, m, m, p, m, p, m, p, m, p, m, m, m, m, m, p, p, p, p, m, p, p, p, p, p, p, p, m, p, p, p, p, m],
+    [m, p, m, p, m, p, p, p, p, m, p, m, p, m, p, p, p, p, p, p, p, e, m, m, m, m, m, p, m, m, m, m, m, p, m, m, m, m, p, m],
+    [m, p, m, p, m, p, m, m, p, m, p, m, p, m, p, m, p, m, p, m, p, m, p, p, m, m, p, p, p, p, m, v, m, p, p, p, p, m, p, m],
+    [m, p, m, p, m, p, p, m, p, m, p, m, p, m, p, m, p, m, p, m, p, m, p, p, m, m, p, m, m, p, p, m, m, m, m, m, p, m, p, m],
+    [m, p, m, p, m, m, m, m, p, m, p, m, p, p, p, p, p, m, p, m, p, m, p, p, p, p, p, e, m, m, p, m, p, p, p, m, p, m, p, m],
+    [m, p, m, p, p, p, p, p, p, m, p, m, m, m, m, m, m, m, p, m, p, p, m, p, m, m, m, p, p, m, p, m, p, m, p, p, p, m, p, m],
+    [m, p, m, p, m, m, m, m, m, m, p, m, e, p, p, p, p, p, p, m, m, p, p, p, m, p, p, p, p, p, p, m, p, m, m, m, m, m, p, m],
+    [m, p, p, p, p, p, p, p, p, p, p, m, m, m, m, m, m, m, m, m, m, m, m, m, p, p, p, p, m, m, m, m, p, m, p, p, p, m, i, m],
+    [m, e, m, m, m, m, m, m, m, m, p, p, p, p, p, p, p, p, p, p, p, p, p, p, p, m, m, m, m, m, p, p, p, p, p, m, m, p, m, m],
+    [m, m, m, v, v, v, v, v, v, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, m, v, v, v, m, m, m, m, m, m, m, m, m, m, m],
+
 ]
